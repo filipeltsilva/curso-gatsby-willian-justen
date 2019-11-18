203256e01c421369bf2e9163e41397d16facc95c
Filipe Lemos <contato@filipelemos.com>
2019-11-18 17:46:50 -0300

    Creating pages and components

---
+++ src/components/Profile/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Profile = () => (
+  <div className="profile-wrapper">
+    <h1>Filipe Lemos</h1>
+    <h2>Desenvolvedor Front-end</h2>
+    <p>CEO da filipelemos.com</p>
+  </div>
+)
+
+export default Profile
